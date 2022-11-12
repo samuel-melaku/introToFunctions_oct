bddcb1e2cf4f53ddc4d45c6e7a3ddb04b58b86c4
Samuel Melaku <samuel.melaku@hgs.hiddengeniusproject.org>
2022-11-12 11:47:50 -0800

    11:47

---
+++ introToFunctions.cpp
@@ -0,0 +1,5 @@
+
+// the main function is called when the program is executed
+int x;
+x = pow(10, 2) * 2;
+// there can be code in the parenthesis or maybe not depending on what the function is for

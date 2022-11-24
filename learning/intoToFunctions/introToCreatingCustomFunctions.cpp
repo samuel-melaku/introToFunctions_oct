53d598d3847ab0e908320c957a4966de969d99bd
Samuel Melaku <samuel.melaku@hgs.hiddengeniusproject.org>
2022-11-24 11:16:35 -0800

    commited

---
+++ learning/intoToFunctions/introToCreatingCustomFunctions.cpp
@@ -0,0 +1,16 @@
+// A function name can also be called an identifier
+// functions make code more scalable
+
+int multiply(int x, int y)
+{
+    return x * y;;
+}
+
+int main() {
+
+
+
+// calling a function does the code inside of the {}
+mutliply(2, 5);
+
+}

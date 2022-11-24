efe675378ef626aff1fccb50a4af9341372a123e
Samuel Melaku <samuel.melaku@hgs.hiddengeniusproject.org>
2022-11-24 12:22:56 -0800

    #

---
+++ learning/intoToFunctions/numericFunctions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << sqrt(25) << endl;
+
+    cout << remainder(10, 3) << endl; // modulo 1
+    cout << 10 % 3 << endl;
+    // ceiling and floor cut decimal values up and down (guess which is which)
+}

b659be093c55972dfb3f4e7f2d22cd15f3e28759
Samuel Melaku <samuel.melaku@hgs.hiddengeniusproject.org>
2022-11-24 15:20:33 -0800

    complicated

---
+++ learning/conditionals/controlFlow.cpp
@@ -0,0 +1,14 @@
+// control flow can either be branching or looping
+//branching is where you do conditions and you'll branch the application to do different things based on results
+// looping will loop the code a cetain number of times
+
+/*
+
+if(expression)     True / False
+{
+   < code>
+} else {
+    <code>
+} else {
+    <code>
+}
